7d65aa6b48580c73e73b5446e517ac3822e2a2cb
amirreza <116113580+amirrezanaseri@users.noreply.github.com>
2023-01-12 19:01:16 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// 6000$ per month
+// 55$ daily
+// 66000$ car
+
+const CostPerMonth = 55 * 30;
+console.log("CostPerMonth: ", CostPerMonth);
+
+const SavingPerMonth = 6000 - CostPerMonth;
+console.log("SavingPerMonth: ", SavingPerMonth);
+
+const WaitingMonth = 66000 / SavingPerMonth;
+console.log("WaitingMonth: ", WaitingMonth);
